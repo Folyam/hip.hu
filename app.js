55c85effb4400c11de364701940ea6c7079b3110
Balazs Nadasdi <yitsushi@gmail.com>
2013-02-02 00:49:00 +0100

    getClientIp from catapulty.tumblr.com

--- app.js
+++ app.js
@@ -88,6 +88,26 @@ everyauth.everymodule.findUserById(function ( userId, callback ) {
   });
 });
 
+// snippet taken from http://catapulty.tumblr.com/post/8303749793/heroku-and-node-js-how-to-get-the-client-ip-address
+function getClientIp(req) {
+  var ipAddress;
+  // The request may be forwarded from local web server.
+  var forwardedIpsStr = req.header('x-forwarded-for');
+  if (forwardedIpsStr) {
+    // 'x-forwarded-for' header may return multiple IP addresses in
+    // the format: "client IP, proxy 1 IP, proxy 2 IP" so take the
+    // the first one
+    var forwardedIps = forwardedIpsStr.split(',');
+    ipAddress = forwardedIps[0];
+  }
+  if (!ipAddress) {
+    // If request was not forwarded
+    ipAddress = req.connection.remoteAddress;
+  }
+  return ipAddress;
+};
+// snippet end
+
 app.configure(function(){
   app.set('port', process.env.PORT || 3000);
   app.set('views', __dirname + '/views');
@@ -99,7 +119,7 @@ app.configure(function(){
   app.use(express.cookieParser('pramboshnypDicOmLertevNocgocUn'));
   app.use(express.session({secret: 'JefthileirrIpsIpHegMyDriheHaid'}));
   app.use(function(req, res, next) {
-    req.session.ip = req.ip;
+    req.session.ip = getClientIp(req);
     return next();
   });
   app.use(everyauth.middleware(app));
