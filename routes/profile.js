a90f2983fdc1021ebdae59bfa301488de7757a93
Balazs Nadasdi <yitsushi@gmail.com>
2013-02-02 13:06:19 +0100

    generate random activation hash

--- routes/profile.js
+++ routes/profile.js
@@ -1,6 +1,8 @@
 var mongoose = require('mongoose');
 var User = mongoose.model('User');
 
+var StringUtils = require('../lib/stringutils');
+
 exports.index = function(req, res, next) {
   if (!req.loggedIn) {
     return next();
@@ -14,6 +16,25 @@ exports.index = function(req, res, next) {
       return next();
     }
 
+    if (typeof user.info.activation_code == "undefined") {
+      return checkAndGenerateUniqActivationCode(StringUtils.generateActivationCode(15), function(err, code) {
+        user.info.activation_code = code;
+        return user.save(function(err, d) {
+          return res.render("profile/index", { user: user });
+        });
+      });
+    }
+
     return res.render("profile/index", { user: user });
   });
 };
+
+var checkAndGenerateUniqActivationCode = function(code, callback) {
+  User.count({info: { activation_code: code }}, function(err, data) {
+    if (data < 1) {
+      return callback(err, code);
+    } else {
+      return checkAndGenerateUniqActivationCode(StringUtils.generateActivationCode(15), callback);
+    }
+  });
+}
