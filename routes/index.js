2c2f6df1ff3314ca54241dd7176b7a85f81aab48
Balazs Nadasdi <yitsushi@gmail.com>
2013-02-04 11:38:43 +0100

    add percentage

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,13 @@
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Hip.hu' });
+  var start   = 1359972947375;
+  var end     = 1360080047375;
+  var current = new Date().getTime();
+
+  var percentage = (1-((end-current)/(end-start)))*100;
+
+  res.render('index', { percentage: percentage });
 };
 exports.error404 = function(req, res) {
   res.render('404', { title: 'Hip.hu' });
