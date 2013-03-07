92567751616cf72f36f8317924bb3bcc80e5bc5f
Balazs Nadasdi <yitsushi@gmail.com>
2013-03-07 13:07:57 +0100

    add round-delay [#45451063]

--- workers/commlog.js
+++ workers/commlog.js
@@ -3,15 +3,22 @@ var mongoose = require('../lib/mongodbsetup');
 var Comm = require('./lib/comm'),
     async = require('async');
 
+var lastReduceRun = 0;
+
 function main() {
   return Comm.sendRequest(Comm.generatePostData(), function(err, messages) {
     if (err) {
       return console.log('problem with request: ' + e.message);
     }
     return async.mapSeries(messages, Comm.checkMessage, function(err, m) {
-      return Comm.execReduces(function() {
+      if (lastReduceRun++ > 29) {
+        return Comm.execReduces(function() {
+          lastReduceRun = 0;
+          return main();
+        });
+      } else {
         return main();
-      });
+      }
     });
   });
 }
